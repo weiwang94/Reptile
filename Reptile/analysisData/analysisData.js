d7e1a9dc0793720c8f2756ecc669b459b5a66407
wei <wei_wang94@sina.com>
2017-04-17 10:32:37 +0800

    修改了 电影存储

--- Reptile/analysisData/analysisData.js
+++ Reptile/analysisData/analysisData.js
@@ -1,10 +1,33 @@
 var log = function() {
 	console.log.apply(console, arguments)
 }
+// NationO 是国家的 名字 和 数量的统计对象
+// { '美国': 140,
+//   '法国': 40,
+//   '中国': 40,
+//   '意大利': 30,
+//   '日本': 20,
+//   '英国': 20,
+//   '印度': 10,
+//   '瑞士': 10,
+//   '德国': 20,
+//   '新西兰': 10 }
 const NationO = {}
+// NationA 是 NationO 对象转化为 供echarts用的 数组 对象
+// [ { name: '美国', value: 140 },
+//   { name: '法国', value: 40 },
+//   { name: '中国', value: 40 },
+//   { name: '意大利', value: 30 },
+//   { name: '日本', value: 20 },
+//   { name: '英国', value: 20 },
+//   { name: '印度', value: 10 },
+//   { name: '瑞士', value: 10 },
+//   { name: '德国', value: 20 },
+//   { name: '新西兰', value: 10 } ]
 const NationA = []
+
 const fs = require('fs')
-var nation = function(option, callback) {
+const nation = function(option, callback) {
 	var path = './movieInfo.txt'
 	fs.readFile(path, option, function(err, data){
 		if(err!=null) {
@@ -16,13 +39,14 @@ var nation = function(option, callback) {
 	})
 }
 
-var divisonString = function(str) {
+const divisonString = function(str) {
 	var s = []
 	var array = str.split(' ')
 	for (var i = 0; i < array.length; i++) {
 		if(array[i] == '中国大陆') {
 			s.push('中国')
 		}else if(array[i] == '香港') {
+			s.push('中国')
 		}else {
 			s.push(array[i])
 		}
@@ -30,7 +54,7 @@ var divisonString = function(str) {
 	return s
 }
 
-var findNation = function(movie) {
+const findNation = function(movie) {
 	var nationArray = []
 	for(let i = 0; i < movie.length; i++) {
 		var str = divisonString(movie[i].nation)
@@ -38,9 +62,12 @@ var findNation = function(movie) {
 			nationArray.push(str[j])
 		}
 	}
+	// nationArray.map(str => {
+	// 	log(str)
+	// })
 	return nationArray
 }
-var nationOCount = function(movieNation) {
+const nationOCount = function(movieNation) {
 	for (var i = 0; i < movieNation.length; i++) {
 		var m = movieNation[i]
 		if(m in NationO) {
@@ -49,11 +76,15 @@ var nationOCount = function(movieNation) {
 			NationO[m] = 1
 		}
 	}
+	log('nationOCount', NationO)
 }
-var nationACount = function() {
+const nationACount = function() {
 	var property
 	for(property in NationO) {
-		var count = {name:'',value: ''}
+		var count = {
+			name:'',
+			value: ''
+		}
 		count.name = property
 		count.value = NationO[property]
 		NationA.push(count)
@@ -61,6 +92,8 @@ var nationACount = function() {
 	log(NationA)
 }
 
+// 调用 nation 函数异步读取 存储有电影信息的文件
+// data 是文件读取完后 回调的参数
 nation('utf8', function(data) {
    var movie = JSON.parse(data)
    var nationArray =  findNation(movie)
